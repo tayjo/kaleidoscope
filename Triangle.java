286252f6d913c617e40427c203a6e36f31a7813e
Josh Taylor <tayjo@seas.upenn.edu>
2014-11-25 16:23:53 -0500

    drawTriangles
    -removed coordinates from Triangle, no longer needed because they are
    calculated when painted
    -added java doc to View

--- Triangle.java
+++ Triangle.java
@@ -3,12 +3,8 @@ package kaleidoscope;
 public class Triangle extends Figure {
 	private int width;
 	private int height;
-	private int[] xCoords;
-	private int[] yCoords;
 	
 	Triangle(int width, int height) {
-		xCoords = new int[3];
-		yCoords = new int[3];
 		this.width = width;
 		this.height = height;
 	}
@@ -23,19 +19,5 @@ public class Triangle extends Figure {
         for (int y = 0; y < getYPosition(); y++) {
         	setYPosition(Math.min(getYPosition(), getYLimit()));
         }
-	}
-	
-	public int[] getXCoords() {
-		xCoords[0] = getXPosition();
-		xCoords[1] = getXPosition() + this.width / 2;
-		xCoords[2] = getXPosition() + this.width;
-		return xCoords;
-	}
-	
-	public int[] getYCoords() {
-		yCoords[0] = getYPosition() + this.height;
-		yCoords[1] = getYPosition();
-		yCoords[2] = getYPosition() + this.height;
-		return yCoords;
 	}
 }
--- View.java
+++ View.java
@@ -12,8 +12,8 @@ import javax.swing.JPanel;
  * In this example, the Model contains only a single bouncing ball.
  * 
  * @author David Matuszek
- * @author Your name goes here
- * @author Your name goes here
+ * @author Josh Taylor
+ * @author Ted Fujimoto
  */
 public class View extends JPanel implements Observer {
     
@@ -48,7 +48,7 @@ public class View extends JPanel implements Observer {
         	thisFigure = model.getFigure(i);
 	        if (thisFigure instanceof Ball) {
 	        	ball = (Ball) thisFigure;
-	        	paintBall(g, ball);
+	        	paintBalls(g, ball);
 	        }
 	        else if (thisFigure instanceof Rectangle) {
 	        	g.fillRect(model.getX(i), model.getY(i),
@@ -56,30 +56,189 @@ public class View extends JPanel implements Observer {
 	        }
 	        else if (thisFigure instanceof Triangle) {
 	        	tri = (Triangle) thisFigure;
-	        	g.fillPolygon(tri.getXCoords(), tri.getYCoords(), 3);
+	        	paintTriangles(g, tri);
 	        }
         }
     }
-
-    private void paintBall(Graphics g, Ball ball) {
+    
+    /**
+     * Paints the 8 different copies of the ball
+     * 
+     * @param g The Graphics on which to paint things.
+     * @param ball The Ball object to be painted (and reflected)
+     */
+    private void paintBalls(Graphics g, Ball ball) {
     	int x = ball.getXPosition();
     	int y = ball.getYPosition();
     	int negX = getNegativeX(x);
     	int negY = getNegativeY(y);
-    	g.fillOval(x, y, model.BALL_SIZE, model.BALL_SIZE);
-    	g.fillOval(negX, y, model.BALL_SIZE, model.BALL_SIZE);
-    	g.fillOval(x, negY, model.BALL_SIZE, model.BALL_SIZE);
-    	g.fillOval(negX, negY, model.BALL_SIZE, model.BALL_SIZE);
-    	g.fillOval(y, x, model.BALL_SIZE, model.BALL_SIZE);
-    	g.fillOval(negY, x, model.BALL_SIZE, model.BALL_SIZE);
-    	g.fillOval(y, negX, model.BALL_SIZE, model.BALL_SIZE);
-    	g.fillOval(negY, negX, model.BALL_SIZE, model.BALL_SIZE);
+    	int size = model.BALL_SIZE;
+    	g.fillOval(x, y, size, size);
+    	g.fillOval(negX - size, y, size, size);
+    	g.fillOval(x, negY - size, size, size);
+    	g.fillOval(negX - size, negY - size, size, size);
+    	g.fillOval(y - size, x - size, size, size);
+    	g.fillOval(negY, x - size, size, size);
+    	g.fillOval(y - size, negX, size, size);
+    	g.fillOval(negY, negX, size, size);
+    }
+   
+    /**
+     * Paints the 8 different copies of the triangle
+     * 
+     * @param g The Graphics on which to paint things.
+     * @param tri The Triangle object to be painted (and reflected)
+     */
+    private void paintTriangles(Graphics g, Triangle tri) {
+    	int x = tri.getXPosition();
+    	int y = tri.getYPosition();
+    	int negX = getNegativeX(x);
+    	int negY = getNegativeY(y);
+    	int width = model.TRI_WIDTH;
+    	int height = model.TRI_HEIGHT;
+    	drawTriangle(g, x, y, "up");
+    	drawTriangle(g, negX - width, y, "up");
+    	drawTriangle(g, x, negY - height, "down");
+    	drawTriangle(g, negX - width, negY - height, "down");
+    	drawTriangle(g, y - height, x - width, "left");
+    	drawTriangle(g, negY, x - width, "right");
+    	drawTriangle(g, y - height, negX, "left");
+    	drawTriangle(g, negY, negX, "right");
+    }
+
+    /**
+     * Calculates the coordinates of a triangle based on the x & y
+     * position of the triangle and the direction that it is pointing,
+     * then paints the triangle. Throws an exception if the direction
+     * provided is not valid.
+     * 
+     * @param g The Graphics on which to paint things.
+     * @param x The x position of the triangle
+     * @param y The y position of the triangle
+     * @param dir The direction that the triangle is pointing
+     * ("up", "down", "left", or "right")
+     */
+    private void drawTriangle(Graphics g, int x, int y, String dir) {
+    	int[] xCoords = new int[3];
+    	int[] yCoords = new int[3];
+    	if (dir == "up") {
+    		getUpCoords(x, y, xCoords, yCoords);
+    	}
+    	else if (dir == "down") {
+    		getDownCoords(x, y, xCoords, yCoords);
+    	}
+    	else if (dir == "left") {
+    		getLeftCoords(x, y, xCoords, yCoords);
+    	}
+    	else if (dir == "right") {
+    		getRightCoords(x, y, xCoords, yCoords);
+    	}
+    	else {
+    		throw new IllegalArgumentException();
+    	}
+    	g.fillPolygon(xCoords, yCoords, 3);
     }
     
+    /**
+     * Calculates the x & y coordinates of the
+     * vertices of an upwards pointing triangle
+     * 
+     * @param x The x position of the triangle.
+     * @param y The y position of the triangle.
+     * @param xCoords An array of size 3 that is loaded
+     * with the x coordinates of the triangle's vertices
+     * @param yCoords An array of size 3 that is loaded
+     * with the y coordinates of the triangle's vertices
+     */
+    private void getUpCoords(int x, int y, int[] xCoords, int[] yCoords) {
+		xCoords[0] = x;
+		xCoords[1] = x + model.TRI_WIDTH / 2;
+		xCoords[2] = x + model.TRI_WIDTH;
+		yCoords[0] = y + model.TRI_HEIGHT;
+		yCoords[1] = y;
+		yCoords[2] = y + model.TRI_HEIGHT;
+    }
+    
+    /**
+     * Calculates the x & y coordinates of the
+     * vertices of a downwards pointing triangle
+     * 
+     * @param x The x position of the triangle.
+     * @param y The y position of the triangle.
+     * @param xCoords An array of size 3 that is loaded
+     * with the x coordinates of the triangle's vertices
+     * @param yCoords An array of size 3 that is loaded
+     * with the y coordinates of the triangle's vertices
+     */
+    private void getDownCoords(int x, int y, int[] xCoords, int[] yCoords) {
+    	xCoords[0] = x;
+		xCoords[1] = x + model.TRI_WIDTH;
+		xCoords[2] = x + model.TRI_WIDTH / 2;
+		yCoords[0] = y;
+		yCoords[1] = y;
+		yCoords[2] = y + model.TRI_HEIGHT;
+    }
+    
+    /**
+     * Calculates the x & y coordinates of the
+     * vertices of a leftwards pointing triangle
+     * 
+     * @param x The x position of the triangle.
+     * @param y The y position of the triangle.
+     * @param xCoords An array of size 3 that is loaded
+     * with the x coordinates of the triangle's vertices
+     * @param yCoords An array of size 3 that is loaded
+     * with the y coordinates of the triangle's vertices
+     */
+    private void getLeftCoords(int x, int y, int[] xCoords, int[] yCoords) {
+    	xCoords[0] = x;
+		xCoords[1] = x + model.TRI_HEIGHT;
+		xCoords[2] = x + model.TRI_HEIGHT;
+		yCoords[0] = y + model.TRI_WIDTH / 2;
+		yCoords[1] = y;
+		yCoords[2] = y + model.TRI_WIDTH;
+    }
+    
+    /**
+     * Calculates the x & y coordinates of the
+     * vertices of a rightwards pointing triangle
+     * 
+     * @param x The x position of the triangle.
+     * @param y The y position of the triangle.
+     * @param xCoords An array of size 3 that is loaded
+     * with the x coordinates of the triangle's vertices
+     * @param yCoords An array of size 3 that is loaded
+     * with the y coordinates of the triangle's vertices
+     */
+    private void getRightCoords(int x, int y, int[] xCoords, int[] yCoords) {
+    	xCoords[0] = x;
+		xCoords[1] = x + model.TRI_HEIGHT;
+		xCoords[2] = x;
+		yCoords[0] = y;
+		yCoords[1] = y + model.TRI_WIDTH / 2;
+		yCoords[2] = y + model.TRI_WIDTH;
+    }
+        
+    /**
+     * Returns the x coordinate on the view that 
+     * corresponds to the negative of the given x
+     * coordinate in a center-origin coordinate system
+     * 
+     * @param x The x coordinate
+     * @return The coordinate corresponding to negative x
+     */
     private int getNegativeX(int x) {
     	return getWidth() - x;
     }
     
+    /**
+     * Returns the y coordinate on the view that 
+     * corresponds to the negative of the given y
+     * coordinate in a center-origin coordinate system
+     * 
+     * @param y The y coordinate
+     * @return The coordinate corresponding to negative y
+     */
     private int getNegativeY(int y) {
     	return getHeight() - y;
     }
